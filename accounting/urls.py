7fd06f010db06676cab7b634003cc78c7e6581d0
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 11:19:49 +0200

    add first urlpatterns to app and project

---
+++ accounting/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from accounting import views
+
+urlpatterns = [
+    path('transactions/',views.transaction_list),
+    path('transactions/<int:pk>/', views.transaction_detail)
+]
