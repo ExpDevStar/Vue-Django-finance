149327b4eaa56661dab05f0e520366703cfe6d4e
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 12:40:37 +0200

    add list and detail views

--- accounting/urls.py
+++ accounting/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from accounting import views
 
 urlpatterns = [
-    path('transactions/',views.transaction_list),
+    path('transactions/', views.transaction_list),
     path('transactions/<int:pk>/', views.transaction_detail)
 ]
--- accounting/views.py
+++ accounting/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Transaction
+
+def transaction_list(request):
+    transactions = Transaction.objects.all()
+    data = {"results": list(transactions.values())}
+    return JsonResponse(data)
+
+def transaction_detail(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    data = {"results": {
+        "amount": transaction.amount,
+        "currency": transaction.currency,
+        "trans_type": transaction.trans_type,
+        "category": transaction.category,
+        "subcategory": transaction.subcategory,
+        "from_account": transaction.from_account,
+        "on_account": transaction.on_account,
+        "create_datetime": transaction.create_datetime,
+        "place": transaction.place,
+        "notes": transaction.notes
+    }}
+    return JsonResponse(data)
