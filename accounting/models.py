063a1cba2cc3d3c88dcd0a945819d8c073486e49
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 10:54:04 +0200

    add Transaction model and run first migrations on it

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+"""Transaction model will be a core of homefinance application
+    Now it has many CharField attributes, which will become ForeignKey
+    in future.
+"""
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    currency = models.CharField(max_length=3)
+    trans_type = models.CharField(max_length=10)
+    category = models.CharField(max_length=10)
+    subcategory = models.CharField(max_length=10)
+    from_account = models.CharField(max_length=10)
+    on_account = models.CharField(max_length=10)
+    create_datetime = models.DateTimeField(default=timezone.now)
+    place = models.CharField(max_length=10)
+    notes = models.TextField()
+
+    def __str__(self):
+        return '{}, {} {}, {}'.format(self.notes, 
+                                        self.amount, 
+                                        self.currency, 
+                                        self.create_datetime
+                                        )
