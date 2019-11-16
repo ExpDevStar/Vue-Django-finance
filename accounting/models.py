c00501740c8d9d4ec587922a3570f3e40c2d800f
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 13:54:05 +0200

    add blank=True to some Transaction model Fields

--- accounting/models.py
+++ accounting/models.py
@@ -10,13 +10,13 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     currency = models.CharField(max_length=3)
     trans_type = models.CharField(max_length=10)
-    category = models.CharField(max_length=10)
-    subcategory = models.CharField(max_length=10)
-    from_account = models.CharField(max_length=10)
-    on_account = models.CharField(max_length=10)
+    category = models.CharField(max_length=10, blank=True)
+    subcategory = models.CharField(max_length=10, blank=True)
+    from_account = models.CharField(max_length=10, blank=True)
+    on_account = models.CharField(max_length=10, blank=True)
     create_datetime = models.DateTimeField(default=timezone.now)
-    place = models.CharField(max_length=10)
-    notes = models.TextField()
+    place = models.CharField(max_length=10, blank=True)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return '{}, {} {}, {}'.format(self.notes, 
