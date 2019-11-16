f4807f58a1a78c592c26132669d9c7aa820bbc25
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 16:51:56 +0200

    replace views on drf modelviewset

--- accounting/views.py
+++ accounting/views.py
@@ -1,25 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
+from rest_framework import viewsets
 
 from .models import Transaction
+from .serializers import TransactionSerializer
 
-def transaction_list(request):
-    transactions = Transaction.objects.all()
-    data = {"results": list(transactions.values())}
-    return JsonResponse(data)
 
-def transaction_detail(request, pk):
-    transaction = get_object_or_404(Transaction, pk=pk)
-    data = {"results": {
-        "amount": transaction.amount,
-        "currency": transaction.currency,
-        "trans_type": transaction.trans_type,
-        "category": transaction.category,
-        "subcategory": transaction.subcategory,
-        "from_account": transaction.from_account,
-        "on_account": transaction.on_account,
-        "create_datetime": transaction.create_datetime,
-        "place": transaction.place,
-        "notes": transaction.notes
-    }}
-    return JsonResponse(data)
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
