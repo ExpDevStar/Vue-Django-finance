61a44b9af775eff5464cd8d85b583e18b688800f
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 17:41:27 +0200

    add some changes

--- homefinance/urls.py
+++ homefinance/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from homefiance.accounting import views
+from accounting import views
 
 router = routers.DefaultRouter()
 router.register(r'transactions', views.TransactionViewSet)
 
 
 urlpatterns = [
-    path('', include('router.urls')),
+    path('', include(router.urls)),
 ]
