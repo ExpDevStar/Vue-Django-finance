be2e22c4cde7554c0cd13df62d7cc818a16affd0
ExpDevStar <expdevstar000@outlook.com>
2019-11-16 16:59:48 +0200

    replace urlpatterns on drf routers

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from accounting import views
 
 urlpatterns = [
-    path('transactions/', views.transaction_list),
-    path('transactions/<int:pk>/', views.transaction_detail)
+
 ]
