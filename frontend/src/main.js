c23b67b53ec3301d393458d02f26c29bce16b841
ExpDevStar <expdevstar000@outlook.com>
2019-11-24 12:05:24 +0200

    import store and bootstrap-vue to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 Vue.use(Vuex)
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
