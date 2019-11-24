872dd64be2600d80037f2b930962a697f19d9292
ExpDevStar <expdevstar000@outlook.com>
2019-11-24 13:52:22 +0200

    add Transaction model to admin

--- homefinance/urls.py
+++ homefinance/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounting import views
@@ -8,4 +9,5 @@ router.register(r'transactions', views.TransactionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
 ]
