26ce9c7946ceab6533edd26342561907ef6d4328
ExpDevStar <expdevstar000@outlook.com>
2019-11-30 21:10:56 +0200

    change inputs to selects

--- frontend/src/components/transaction/createTransactionForm.vue
+++ frontend/src/components/transaction/createTransactionForm.vue
@@ -11,37 +11,37 @@
             </b-form-group>
 
             <b-form-group label="Currency" label-for="currency">
-                <b-form-input 
+                <b-form-select 
                     id="currency" 
                     v-model="form.currency"
-                    type="text"
+                    :options="getCurrencies"
                     required
-                ></b-form-input>
+                ></b-form-select>
             </b-form-group>
 
             <b-form-group label="Transaction Type" label-for="trans_type">
-                <b-form-input 
+                <b-form-select 
                     id="trans_type" 
                     v-model="form.trans_type"
-                    type="text"
+                    :options="transactionTypes"
                     required
-                ></b-form-input>
+                ></b-form-select>
             </b-form-group>
 
             <b-form-group label="Category" label-for="category">
-                <b-form-input 
+                <b-form-select 
                     id="category" 
                     v-model="form.category"
-                    type="text"
-                ></b-form-input>
+                    :options="getCategories"
+                ></b-form-select>
             </b-form-group>
 
             <b-form-group label="Subcategory" label-for="subcategory">
-                <b-form-input 
+                <b-form-select 
                     id="subcategory" 
                     v-model="form.subcategory"
-                    type="text"
-                ></b-form-input>
+                    :options="getSubcategories"
+                ></b-form-select>
             </b-form-group>
 
             <b-form-group label="From account" label-for="from_account">
@@ -69,11 +69,11 @@
             </b-form-group>
 
             <b-form-group label="Place" label-for="place">
-                <b-form-input 
+                <b-form-select 
                     id="place" 
                     v-model="form.place"
-                    type="text"
-                ></b-form-input>
+                    :options="getPlaces"
+                ></b-form-select>
             </b-form-group>
             <b-form-group label="Notes" label-for="notes">
                 <b-form-textarea 
@@ -105,6 +105,11 @@ export default {
                 place: '',
                 notes: '',
             },
+            transactionTypes: [
+                { value: 'INC', text: 'Income'},
+                { value: 'EXP', text: 'Expences'},
+                { value: 'TEC', text: 'Technical'},
+            ],
         }
     },
     computed: {
@@ -115,13 +120,69 @@ export default {
                 rawAccounts.forEach((rawAccount) => {
                     const account = {
                         value: rawAccount.id,
-                        text: `${rawAccount.title}, ${rawAccount.amount} ${rawAccount.currency}` 
+                        text: `${rawAccount.title}, ${rawAccount.amount}` 
                     };
                     accounts.push(account);
                 })
             }
             return accounts
-        }
+        },
+        getCurrencies() {
+            const rawCurrencies = this.$store.getters.currencies;
+            const currencies = [];
+            if (rawCurrencies) {
+                rawCurrencies.forEach((rawCurrency) => {
+                    const currency = {
+                        value: rawCurrency.id,
+                        text: rawCurrency.name,
+                    };
+                    currencies.push(currency);
+                })
+            }
+            return currencies
+        },
+        getCategories() {
+            const rawCategories = this.$store.getters.categories;
+            const categories = [];
+            if (rawCategories) {
+                rawCategories.forEach((rawCategory) => {
+                    const category = {
+                        value: rawCategory.id,
+                        text: rawCategory.name,
+                    };
+                    categories.push(category);
+                })
+            }
+            return categories
+        },
+        getSubcategories() {
+            const rawSubcategories= this.$store.getters.subcategories;
+            const subcategories = [];
+            if (rawSubcategories) {
+                rawSubcategories.forEach((rawSubcategory) => {
+                    const subcategory = {
+                        value: rawSubcategory.id,
+                        text: rawSubcategory.name,
+                    };
+                    subcategories.push(subcategory);
+                })
+            }
+            return subcategories
+        },
+        getPlaces() {
+            const rawPlaces= this.$store.getters.places;
+            const places = [];
+            if (rawPlaces) {
+                rawPlaces.forEach((rawPlace) => {
+                    const place = {
+                        value: rawPlace.id,
+                        text: rawPlace.name,
+                    };
+                    places.push(place);
+                })
+            }
+            return places
+        },
     },
     methods: {
         onSubmit(evt) {
@@ -144,6 +205,10 @@ export default {
     },
     beforeMount() {
         this.$store.dispatch('getAccounts');
+        this.$store.dispatch('getCurrencies');
+        this.$store.dispatch('getCategories');
+        this.$store.dispatch('getSubcategories');
+        this.$store.dispatch('getPlaces');
     }
 }
 </script>
