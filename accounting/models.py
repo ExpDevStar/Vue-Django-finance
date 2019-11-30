ee7f03953d77dd045c4fce4536fd78bb6940e657
ExpDevStar <expdevstar000@outlook.com>
2019-11-30 11:18:31 +0200

    add additional models: currency, account, place

--- accounting/models.py
+++ accounting/models.py
@@ -2,20 +2,22 @@ from django.db import models
 from django.utils import timezone
 
 
-"""Transaction model will be a core of homefinance application
-    Now it has many CharField attributes, which will become ForeignKey
-    in future.
-"""
+TRANSACTION_TYPES = [
+        ('INC', 'Income'),
+        ('EXP', 'Expences'),
+        ('TEC', 'Technical')
+    ]
+
 class Transaction(models.Model):
     amount = models.DecimalField(max_digits=7, decimal_places=2)
-    currency = models.CharField(max_length=3)
-    trans_type = models.CharField(max_length=3)
-    category = models.CharField(max_length=10, blank=True)
-    subcategory = models.CharField(max_length=10, blank=True)
-    from_account = models.CharField(max_length=10, blank=True)
-    on_account = models.CharField(max_length=10, blank=True)
+    currency = models.ForeignKey('Currency', on_delete=models.PROTECT)
+    trans_type = models.CharField(max_length=3, choices=TRANSACTION_TYPES)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    subcategory = models.ForeignKey('Subcategory', on_delete=models.PROTECT)
+    from_account = models.ForeignKey('Account', related_name="from_account", on_delete=models.PROTECT)
+    on_account = models.ForeignKey('Account', related_name="on_account", on_delete=models.PROTECT)
     create_datetime = models.DateTimeField(default=timezone.now)
-    place = models.CharField(max_length=10, blank=True)
+    place = models.ForeignKey('Place', on_delete=models.PROTECT)
     notes = models.TextField(blank=True)
 
     def __str__(self):
@@ -24,3 +26,44 @@ class Transaction(models.Model):
                                         self.currency, 
                                         self.create_datetime
                                         )
+
+
+class Account(models.Model):
+    title = models.CharField(max_length=20)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    currency = models.ForeignKey('Currency', on_delete=models.PROTECT)
+    notes = models.TextField(blank=True)
+    create_datetime = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '{}, {} {}'.format(self.title, self.amount, self.currency)
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=3)
+    full_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    cat_type = models.CharField(max_length=3, choices=TRANSACTION_TYPES)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=20)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{}, {}'.format(self.name, self.category)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.name)
