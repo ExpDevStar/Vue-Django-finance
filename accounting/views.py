3639fba4d595f392f79a2e047976d598b0ebee14
ExpDevStar <expdevstar000@outlook.com>
2019-11-30 15:21:24 +0200

    add viewsets for account, currency, place, category

--- accounting/views.py
+++ accounting/views.py
@@ -1,9 +1,30 @@
 from rest_framework import viewsets
 
-from .models import Transaction
-from .serializers import TransactionSerializer
+from accounting import models
+from accounting import serializers
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
+    queryset = models.Transaction.objects.all()
+    serializer_class = serializers.TransactionSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = models.Account.objects.all()
+    serializer_class = serializers.AccountSerializer
+
+class CurrencyViewSet(viewsets.ModelViewSet):
+    queryset = models.Currency.objects.all()
+    serializer_class = serializers.CurrencySerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+class SubcategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Subcategory.objects.all()
+    serializer_class = serializers.SubcategorySerializer
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = models.Place.objects.all()
+    serializer_class = serializers.PlaceSerializer
