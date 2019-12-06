0fbc0838d9377e45878901cf61ca4bf2937253cf
ExpDevStar <expdevstar000@outlook.com>
2019-12-06 23:40:30 +0200

    create custom create action on transaction viewset

--- accounting/views.py
+++ accounting/views.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
 
 from accounting import models
 from accounting import serializers
@@ -16,6 +19,45 @@ class TransactionViewSet(viewsets.ModelViewSet):
             return self.serializer_action_class[self.action]
         except (KeyError, AttributeError):
             return super().get_serializer_class()
+
+    def create(self, request):
+        amount = Decimal(request.data['amount'])
+        currency = request.data['currency']
+        trans_type = request.data['trans_type']
+        if trans_type == 'INC':
+            on_account = models.Account.objects.get(pk=request.data['on_account'])
+            try:
+                on_account.increase(amount, currency)
+            except Exception as error:
+                return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
+            else:
+                on_account.save()
+        if trans_type == 'EXP':
+            from_account = models.Account.objects.get(pk=request.data['from_account'])
+            try:
+                from_account.decrease(amount, currency)
+            except Exception as error:
+                return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
+            else:
+                from_account.save()
+        if trans_type == 'TEC':
+            from_account = models.Account.objects.get(pk=request.data['from_account'])
+            on_account = models.Account.objects.get(pk=request.data['on_account'])
+            try:
+                from_account.decrease(amount, currency)
+            except Exception as error:
+                return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
+            else:
+                try:
+                    on_account.increase(amount, currency)
+                except Exception as error:
+                    return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    from_account.save()
+                    on_account.save()
+        
+        return super().create(request)
+                
 
 
 class AccountViewSet(viewsets.ModelViewSet):
