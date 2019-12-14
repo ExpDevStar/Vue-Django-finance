0f3364390489c64e514f4abfc78cfb9eb94c120f
ExpDevStar <expdevstar000@outlook.com>
2019-12-14 20:02:43 +0200

    create entry about transaction in account journal

--- accounting/signals.py
+++ accounting/signals.py
@@ -1,9 +1,23 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Account, AccountJournal
+from accounting import models
 
-
-@receiver(post_save, sender=Account)
-def create_account_journal_entry(sender, instance, **kwargs):
-    AccountJournal.objects.create(account=instance, amount=instance.amount)
+@receiver(post_save, sender=models.Transaction)
+def add_transaction_to_account_journal_entry(sender, instance, **kwargs):
+    if instance.trans_type == 'INC':
+        entry = models.AccountJournal.objects.latest('timestamp')
+        entry.transaction = instance
+        entry.save()
+    if instance.trans_type == 'EXP':
+        entry = models.AccountJournal.objects.latest('timestamp')
+        entry.transaction = instance
+        entry.save()
+    if instance.trans_type == 'TEC':
+        from_entry = models.AccountJournal.objects.all()[1]
+        on_entry = models.AccountJournal.objects.latest('timestamp')
+        from_entry.transaction = instance
+        on_entry.transaction = instance
+        from_entry.save()
+        on_entry.save()
+        
